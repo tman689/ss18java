0534cb46e4425dcbae6eeaeb240c0f5344f82415
tman689 <tranminhan05092000@gmail.com>
2025-03-19 15:47:24 +0700

    Update main.js

--- ss18/BT6/main.js
+++ ss18/BT6/main.js
@@ -1,6 +1,6 @@
 let year = parseInt(prompt("Nhập năm bạn muốn:"));
 if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
     document.writeln(" là năm nhuận.");
-} else{
+}else{
     document.writeln(" không phải là năm nhuận.");
 }
