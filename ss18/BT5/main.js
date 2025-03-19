46ddd9f4cd550cadeb6efb30a6c3504348ebb616
tman689 <tranminhan05092000@gmail.com>
2025-03-19 15:47:52 +0700

    Update main.js

--- ss18/BT5/main.js
+++ ss18/BT5/main.js
@@ -1,13 +1,10 @@
 let sonamkinhngiem = parseInt(prompt("Nhập số năm kinh nghiệm của bạn :"));
 if(sonamkinhngiem >= 6){
     document.writeln("quản lý");
-}
-else if(sonamkinhngiem >= 4){
+}else if(sonamkinhngiem >= 4){
     document.writeln("Chuyên viên");
-}
-else if(sonamkinhngiem >= 1){
+}else if(sonamkinhngiem >= 1){
     document.writeln("Nhân viên có kinh nghiệm");
-}
-else{
+}else{
     document.writeln("Mới vào nghề");
 }
